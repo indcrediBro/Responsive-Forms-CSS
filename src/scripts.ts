1418315d714ee2ff22c70765ab2b757c784e1937
indcrediBro <152566488+indcrediBro@users.noreply.github.com>
2023-12-27 17:48:48 +0530

    basic form setup

--- src/scripts.ts
+++ src/scripts.ts
@@ -1,3 +1,15 @@
-import { logDate } from "./logDate";
+import { logDate } from './logDate';
 
 logDate();
+
+const toggleButton = document.getElementById('toggle-mode') as HTMLButtonElement;
+const content = document.querySelector('.rf__main') as HTMLElement;
+const panel = document.querySelector('.main__panel') as HTMLElement;
+
+let isDarkMode = false;
+
+toggleButton.addEventListener('click', () => {
+  isDarkMode = !isDarkMode;
+  content.classList.toggle('dark-mode', isDarkMode);
+  panel.classList.toggle('dark-mode', isDarkMode);
+});
