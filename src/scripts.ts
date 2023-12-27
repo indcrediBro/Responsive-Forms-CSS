a71bad0c0cc246d50975ad2a965dfde384c7922f
indcrediBro <152566488+indcrediBro@users.noreply.github.com>
2023-12-28 03:39:20 +0530

    page 1 PC done

--- src/scripts.ts
+++ src/scripts.ts
@@ -5,6 +5,9 @@ logDate();
 const toggleButton = document.getElementById('toggle-mode') as HTMLButtonElement;
 const content = document.querySelector('.rf__main') as HTMLElement;
 const panel = document.querySelector('.main__panel') as HTMLElement;
+const btns = document.querySelectorAll('.btn');
+const google = document.querySelector('.google') as HTMLElement;
+const facebook = document.querySelector('.facebook') as HTMLElement;
 
 let isDarkMode = false;
 
@@ -12,4 +15,10 @@ toggleButton.addEventListener('click', () => {
   isDarkMode = !isDarkMode;
   content.classList.toggle('dark-mode', isDarkMode);
   panel.classList.toggle('dark-mode', isDarkMode);
+  google.classList.toggle('dark-mode', isDarkMode);
+  facebook.classList.toggle('dark-mode', isDarkMode);
+  for (const b of btns) {
+    if (isDarkMode) b.classList.add('dark-mode');
+    if (!isDarkMode) b.classList.remove('dark-mode');
+  }
 });
