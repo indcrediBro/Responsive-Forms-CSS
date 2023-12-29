f66517c9a294b4220a7deae1a082d6cd18d26161
indcrediBro <152566488+indcrediBro@users.noreply.github.com>
2023-12-29 14:35:54 +0530

    sahjgfh/k;

--- src/scripts.ts
+++ src/scripts.ts
@@ -8,6 +8,7 @@ const panel = document.querySelector('.main__panel') as HTMLElement;
 const btns = document.querySelectorAll('.btn');
 const google = document.querySelector('.google') as HTMLElement;
 const facebook = document.querySelector('.facebook') as HTMLElement;
+const toggles = document.querySelectorAll('.toggle');
 
 let isDarkMode = false;
 
@@ -21,4 +22,13 @@ toggleButton.addEventListener('click', () => {
     if (isDarkMode) b.classList.add('dark-mode');
     if (!isDarkMode) b.classList.remove('dark-mode');
   }
+  for (const t of toggles) {
+    if (isDarkMode) t.classList.add('dark-mode');
+    if (!isDarkMode) t.classList.remove('dark-mode');
+  }
+});
+
+const form = document.querySelector('form');
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
 });
